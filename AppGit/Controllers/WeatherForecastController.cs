ded33a7fb9487ae6206309c06581916bcb719961
Anthony De Oliveira <anthony_deoliveira@outlook.fr>
2024-09-14 18:24:02 +0200

    test push

--- AppGit/Controllers/WeatherForecastController.cs
+++ AppGit/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AppGit.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Fucking Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
